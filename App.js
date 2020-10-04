63c47d06dde9c9015c6493e7df9a71cc52574be1
samuel112233 <samuelbhowmik@gmail.com>
2020-10-04 12:12:42 +0530

    Making a story hub part1

--- App.js
+++ App.js
@@ -1,15 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer} from 'react-navigation';
+import WriteScreen from './screens/WriteScreen';
+import ReadScreen from './screens/ReadScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Apps extends React.Component{
+  render(){
+    return(
+      <View>
+
+      </View>
+    )
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  write : {screen:WriteScreen},
+  read : {screen:ReadScreen}
+},
+
+{
+  defaultNavigationOptions : ({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName = navigation.state.routeName
+      if(routeName == "WriteStory"){
+          return(
+            <Image  
+            source={require("./assets/write.png") 
+            style = {{width:50, height:50}} />
+          )
+      }
+      else if(routeName === "Read"){
+        return(
+          <Image  
+          source={require('./assets/read.png')
+          style = {{width:40,height:40}} />
+        )         
+      }
+    }
+  })
+}
+
+
+
+)
 
 const styles = StyleSheet.create({
   container: {
